25e3528d9a05b07508c61f843175edb9c8379343
khuggins1 <70423368+khuggins1@users.noreply.github.com>
2021-03-02 16:44:15 -0800

    Add files via upload

--- Professional Readme/ProfessionalReadme/Develop/index.js
+++ Professional Readme/ProfessionalReadme/Develop/index.js
@@ -1,11 +1,16 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
-
+fs.writeFile('ReadMe.md', generateMarkdown, err => {
+    if(err) throw err;
+    console.log('Your ReadMe was succesfully created!');
+});
 // TODO: Create a function to initialize app
 function init() {}
 
